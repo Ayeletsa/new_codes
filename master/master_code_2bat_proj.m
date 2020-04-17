8605530fc3d16df77337b4f79183c54ef1a07dfc
ayeletsa <ayeletsarel@gmail.com>
2020-04-17 13:24:38 +0300

    added new functions for plots

--- master/master_code_2bat_proj.m
+++ master/master_code_2bat_proj.m
@@ -4,23 +4,29 @@ clear
 param_folder='D:\Ayelet\2bat_proj\Analysis\new_code\params\';
 Twobatproj_param(param_folder)
 
-%% 1. analyze behavior 
+
 behav_param_file_name=fullfile(param_folder,'behav_params.mat');
 dir_param_file_name=fullfile(param_folder,'dirs_params.mat');
 solo_param_file_name=fullfile(param_folder,'solo_params.mat');
 co_param_file_name=fullfile(param_folder,'co_params.mat');
-
+population_param_file_name=fullfile(param_folder,'co_population_params.mat');
+co_shuffle_param_file_name=fullfile(param_folder,'co_shuffle_params.mat');
+field_param_file_name=fullfile(param_folder,'fields_params.mat');
+%% 1. analyze behavior 
 create_behavior_structs(behav_param_file_name,dir_param_file_name)
 %% 2. initial CO and solo anlysis
-initial_co_solo_analysis(behav_param_file_name,dir_param_file_name,solo_param_file_name,co_param_file_name)
+initial_co_solo_analysis(behav_param_file_name,dir_param_file_name,solo_param_file_name,co_param_file_name,field_param_file_name)
 %% 3. CO shuffling
-co_shuffle_param_file_name=fullfile(param_folder,'co_shuffle_params.mat');
-
 co_shuffling_new(co_shuffle_param_file_name)
 
 %% 4. Solo shuffling
 
 
 %% 5. CO figures
-population_param_file_name=fullfile(param_folder,'co_population_params.mat');
+
 plot_co_main_cell_fig(dir_param_file_name,population_param_file_name)
+%% 6. co figure per field time
+plot_co_main_cell_fig_time_per_field(dir_param_file_name,population_param_file_name,co_param_file_name)
+
+%% 7.CO figure with per inter field analysis
+plot_co_main_cell_fig_inter_field(dir_param_file_name,population_param_file_name)
