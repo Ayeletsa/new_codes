f73dc67240f9d3a3442f75c4fb7a832206bce834
ayeletsa <ayeletsarel@gmail.com>
2020-05-19 13:43:00 +0300

    compute also for bins with nan intersection to remove data there

--- Population_analysis/PV_get_y_intersect_data.m
+++ Population_analysis/PV_get_y_intersect_data.m
@@ -9,11 +9,13 @@ for dir_i=1:2
     for bin_i=1:length(allo_X_bins_vector_of_centers)
         range_intersect=PV_find_y_intersect_range(data,bin_i,allo_X_bins_vector,dir_i);
         
-        if sum(isnan(range_intersect))~=2
+        %if sum(isnan(range_intersect))~=2
             % find ind out of intersection:
             %---------------------------------
             ind_intersect=PV_find_ind_data_out_of_intersect_y(ind_intersect,data,range_intersect,allo_X_bins_vector,bin_i,dir_i);
-        end
+%         else
+%             % if there is no intersection 
+       % end
     end
     %% remove data:
     data=PV_remove_non_intersect_data(data,dir_i,ind_intersect);
