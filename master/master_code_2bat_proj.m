0c9ed12c66814865113e57863fb41301c137329c
ayeletsa <ayeletsarel@gmail.com>
2020-06-03 13:08:35 +0300

    small changes

--- master/master_code_2bat_proj.m
+++ master/master_code_2bat_proj.m
@@ -4,7 +4,6 @@ clear
 param_folder='D:\Ayelet\2bat_proj\Analysis\new_code\params\';
 Twobatproj_param(param_folder)
 
-
 behav_param_file_name=fullfile(param_folder,'behav_params.mat');
 dir_param_file_name=fullfile(param_folder,'dirs_params.mat');
 solo_param_file_name=fullfile(param_folder,'solo_params.mat');
@@ -13,6 +12,7 @@ population_param_file_name=fullfile(param_folder,'co_population_params.mat');
 co_shuffle_param_file_name=fullfile(param_folder,'co_shuffle_params.mat');
 field_param_file_name=fullfile(param_folder,'fields_params.mat');
 per_field_param_file_name=fullfile(param_folder,'per_field_params.mat');
+
 population_vector_param_file_name=fullfile(param_folder,'population_vector_params.mat');
 %% 1. analyze behavior 
 create_behavior_structs(behav_param_file_name,dir_param_file_name)
@@ -30,5 +30,5 @@ plot_co_main_cell_fig(co_param_file_name,dir_param_file_name,population_param_fi
 plot_co_main_cell_fig_time_per_field(dir_param_file_name,population_param_file_name,co_param_file_name,per_field_param_file_name)
 
 %% 7.CO figure with per inter field analysis
-plot_co_main_cell_fig_inter_field(dir_param_file_name,population_param_file_name)
+plot_co_main_cell_fig_new_inter_field(dir_param_file_name,population_param_file_name,co_param_file_name,per_field_param_file_name)
 
