0c2bc7af658b1246a5c500dbf7e9dcddf8a897dd
ayeletsa <ayeletsarel@gmail.com>
2020-06-03 13:09:37 +0300

    changes function of SI computation

--- co/per_field/per_field_prop.m
+++ co/per_field/per_field_prop.m
@@ -6,4 +6,5 @@ std_r=nanstd(r);
 mean_r=nanmean(r);
 per_field(field_i).cv=std_r/mean_r;
 per_field(field_i).modulation_depth=(max(r)-min(r))./(max(r));
-per_field(field_i).SI = fn_compute_spatial_info (p,r);
+[per_field(field_i).SI, ~] = computeSI(r,p);
+
