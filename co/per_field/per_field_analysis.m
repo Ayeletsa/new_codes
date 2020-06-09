ff86c48713786de67a3115af070f3cd0b917057e
ayeletsa <ayeletsarel@gmail.com>
2020-06-09 14:32:20 +0300

    compute some things only for time so added a flag for distnace

--- co/per_field/per_field_analysis.m
+++ co/per_field/per_field_analysis.m
@@ -12,11 +12,12 @@ for field_i=1:size(field_edges,2)
         %per field for distance:
         %=============================================
         %1. compute tuning:
+        distance=1;
         [timespent_binned_new_smooth, ~, ~, tuning_dis_x_fr_per_field, ~,~] ...
             = fn_compute_generic_1D_tuning_new_smooth_new_again ...
             (bsp_vec,spikes_vec,dis_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second, 0,0,0,old_smooth,smooth_window,smooth_type,smooth_tol);
         %2. run shuffle:
-        dis_signif_field=shuffling_per_field_analysis_new_smooth(bsp_vec,spikes_vec,dis_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second,num_shuffles_per_field,alpha_val,old_smooth,smooth_window,smooth_type,smooth_tol,width_at_heigth,min_dis_pos_neg);
+        dis_signif_field=shuffling_per_field_analysis_new_smooth(bsp_vec,spikes_vec,dis_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second,num_shuffles_per_field,alpha_val,old_smooth,smooth_window,smooth_type,smooth_tol,width_at_heigth,min_dis_pos_neg,distance);
         %3. compute properties:
         per_field=per_field_prop(per_field,field_i,tuning_dis_x_fr_per_field,timespent_binned_new_smooth);
         %4. save data to struct:
@@ -26,6 +27,7 @@ for field_i=1:size(field_edges,2)
         
         %per field for time:
         %=============================================
+        distance=0;
         spikes_vec=spikes.time_to_co(find(spikes.x_pos>field_edges(1,field_i) & spikes.x_pos<field_edges(2,field_i)));
         bsp_vec=bsp.time_to_co(find(bsp.x_pos>field_edges(1,field_i) & bsp.x_pos<field_edges(2,field_i)));
                  
@@ -34,7 +36,7 @@ for field_i=1:size(field_edges,2)
             = fn_compute_generic_1D_tuning_new_smooth_new_again ...
             (bsp_vec,spikes_vec,time_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second, 0,0,0,old_smooth,smooth_window,smooth_type,smooth_tol);
         %2. run shuffle:
-        time_signif_field=shuffling_per_field_analysis_new_smooth(bsp_vec,spikes_vec,time_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second,num_shuffles_per_field,alpha_val,old_smooth,smooth_window,smooth_type,smooth_tol,width_at_heigth,min_dis_pos_neg);
+        time_signif_field=shuffling_per_field_analysis_new_smooth(bsp_vec,spikes_vec,time_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second,num_shuffles_per_field,alpha_val,old_smooth,smooth_window,smooth_type,smooth_tol,width_at_heigth,min_dis_pos_neg,distance);
         %3. save data to struct:        
         per_field(field_i).time_signif_field=time_signif_field;
         per_field(field_i).tuning_time_fr_per_field=tuning_time_fr_per_field;
