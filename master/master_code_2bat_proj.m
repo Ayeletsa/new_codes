d2f813fd56d752a6f01b1a592b6b5746dbe83f8b
ayeletsa <ayeletsarel@gmail.com>
2020-06-16 11:07:24 +0300

    added solo shuffle

--- master/master_code_2bat_proj.m
+++ master/master_code_2bat_proj.m
@@ -13,18 +13,17 @@ co_shuffle_param_file_name=fullfile(param_folder,'co_shuffle_params.mat');
 solo_shuffle_param_file_name=fullfile(param_folder,'solo_shuffle_params.mat');
 field_param_file_name=fullfile(param_folder,'fields_params.mat');
 per_field_param_file_name=fullfile(param_folder,'per_field_params.mat');
-
 population_vector_param_file_name=fullfile(param_folder,'population_vector_params.mat');
+inclusion_cells_params_param_file_name=fullfile(param_folder,'inclusion_cells_params.mat');
+
 %% 1. analyze behavior 
 create_behavior_structs(behav_param_file_name,dir_param_file_name)
 %% 2. initial CO and solo anlysis
 initial_co_solo_analysis(behav_param_file_name,dir_param_file_name,solo_param_file_name,co_param_file_name,field_param_file_name,per_field_param_file_name)
 %% 3. CO shuffling
 co_shuffling_new(co_shuffle_param_file_name)
-
 %% 4. Solo shuffling
-
-
+solo_shuffling(solo_shuffle_param_file_name,field_param_file_name)
 %% 5. CO figures
 plot_co_main_cell_fig(co_param_file_name,dir_param_file_name,population_param_file_name,per_field_param_file_name)
 %% 6. co figure per field time
@@ -33,3 +32,6 @@ plot_co_main_cell_fig_time_per_field(dir_param_file_name,population_param_file_n
 %% 7.CO figure with per inter field analysis
 plot_co_main_cell_fig_new_inter_field(dir_param_file_name,population_param_file_name,co_param_file_name,per_field_param_file_name)
 
+
+%% create inclustion lists:
+calc_inclusion_criteria(inclusion_cells_params_param_file_name)
