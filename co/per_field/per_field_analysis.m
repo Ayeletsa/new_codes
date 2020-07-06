de46172067c9254de66c83a24d3db37d82c50099
ayeletsa <ayeletsarel@gmail.com>
2020-07-06 14:07:12 +0300

    bug correction

--- co/per_field/per_field_analysis.m
+++ co/per_field/per_field_analysis.m
@@ -14,9 +14,13 @@ for field_i=1:size(field_edges,2)
         %1. compute tuning:
         distance=1;
         [timespent_binned_new_smooth, ~, ~, tuning_dis_x_fr_per_field, ~,~] ...
-            = fn_compute_generic_1D_tuning_new_smooth_new_again ...
-            (bsp_vec,spikes_vec,dis_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second, 0,0,0,old_smooth,smooth_window,smooth_type,smooth_tol);
-        %2. run shuffle:
+            = fn_compute_generic_1D_tuning_new_smooth ...
+            (bsp_vec,spikes_vec,dis_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second, 0,0,0);
+
+%         [timespent_binned_new_smooth, ~, ~, tuning_dis_x_fr_per_field, ~,~] ...
+%             = fn_compute_generic_1D_tuning_new_smooth_new_again ...
+%             (bsp_vec,spikes_vec,dis_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second, 0,0,0,old_smooth,smooth_window,smooth_type,smooth_tol);
+%         %2. run shuffle:
         dis_signif_field=shuffling_per_field_analysis_new_smooth(bsp_vec,spikes_vec,dis_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second,num_shuffles_per_field,alpha_val,old_smooth,smooth_window,smooth_type,smooth_tol,width_at_heigth,min_dis_pos_neg,distance);
         %3. compute properties:
         per_field=per_field_prop(per_field,field_i,tuning_dis_x_fr_per_field,timespent_binned_new_smooth);
@@ -32,9 +36,10 @@ for field_i=1:size(field_edges,2)
         bsp_vec=bsp.time_to_co(find(bsp.x_pos>field_edges(1,field_i) & bsp.x_pos<field_edges(2,field_i)));
                  
         %1. compute tuning:
-        [~, ~, ~, tuning_time_fr_per_field, ~,~] ...
-            = fn_compute_generic_1D_tuning_new_smooth_new_again ...
-            (bsp_vec,spikes_vec,time_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second, 0,0,0,old_smooth,smooth_window,smooth_type,smooth_tol);
+          [~, ~, ~, tuning_time_fr_per_field, ~,~] ...
+            = fn_compute_generic_1D_tuning_new_smooth ...
+            (bsp_vec,spikes_vec,time_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second, 0,0,0);
+
         %2. run shuffle:
         time_signif_field=shuffling_per_field_analysis_new_smooth(bsp_vec,spikes_vec,time_per_field_bin_vec_of_center, time_spent_minimum_for_1D_bins_per_field, frames_per_second,num_shuffles_per_field,alpha_val,old_smooth,smooth_window,smooth_type,smooth_tol,width_at_heigth,min_dis_pos_neg,distance);
         %3. save data to struct:        
@@ -49,7 +54,6 @@ for field_i=1:size(field_edges,2)
         per_field(field_i).tuning_dis_x_fr_per_field=nan*zeros(1,length(time_per_field_bin_vec_of_center));
     end
 end
-
 %% compute corrs per cell:
 % run only if there is more than one field
 if size(field_edges,2)>1
