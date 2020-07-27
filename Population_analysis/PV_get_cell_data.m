77f57f16773327420422080c407722023c8ae2be
ayeletsa <ayeletsarel@gmail.com>
2020-07-27 09:31:20 +0300

    c

--- Population_analysis/PV_get_cell_data.m
+++ Population_analysis/PV_get_cell_data.m
@@ -13,16 +13,17 @@ data(dir_i).solo.spikes.flight_ts=cell_co_solo_initial_analysis.solo(dir_i).spik
 
 %load co data:
 %bsp:
-data(dir_i).co.bsp.flight_ts=cell_co_solo_initial_analysis.co(dir_i).bsp.ts_usec;
-data(dir_i).co.bsp.flight_x_pos=cell_co_solo_initial_analysis.co(dir_i).bsp.x_pos;
-data(dir_i).co.bsp.flight_y_pos=cell_co_solo_initial_analysis.co(dir_i).bsp.y_pos;
-data(dir_i).co.bsp.flight_dis=cell_co_solo_initial_analysis.co(dir_i).bsp.dis_m;
+data(dir_i).co.bsp.flight_ts=cell_co_solo_initial_analysis.co(dir_i).bsp_full.ts_usec;
+data(dir_i).co.bsp.flight_x_pos=cell_co_solo_initial_analysis.co(dir_i).bsp_full.x_pos;
+data(dir_i).co.bsp.flight_y_pos=cell_co_solo_initial_analysis.co(dir_i).bsp_full.y_pos;
+data(dir_i).co.bsp.flight_dis=cell_co_solo_initial_analysis.co(dir_i).bsp_full.dis_m;
 
 %spikes:
-data(dir_i).co.spikes.flight_ts=cell_co_solo_initial_analysis.co(dir_i).spikes.ts_usec;
-data(dir_i).co.spikes.flight_x_pos=cell_co_solo_initial_analysis.co(dir_i).spikes.x_pos;
-data(dir_i).co.spikes.flight_y_pos=cell_co_solo_initial_analysis.co(dir_i).spikes.y_pos;
-data(dir_i).co.spikes.flight_dis=cell_co_solo_initial_analysis.co(dir_i).spikes.dis_m;
+data(dir_i).co.spikes.flight_ts=cell_co_solo_initial_analysis.co(dir_i).spikes_full.ts_usec;
+data(dir_i).co.spikes.flight_x_pos=cell_co_solo_initial_analysis.co(dir_i).spikes_full.x_pos;
+data(dir_i).co.spikes.flight_y_pos=cell_co_solo_initial_analysis.co(dir_i).spikes_full.y_pos;
+data(dir_i).co.spikes.flight_dis=cell_co_solo_initial_analysis.co(dir_i).spikes_full.dis_m;
 
-
+% velocity short distance prop to medien
+data(dir_i).co.bsp.vel_prop_to_median=cell_co_solo_initial_analysis.co(dir_i).info.vel.vel_prop_to_median ; 
 end
