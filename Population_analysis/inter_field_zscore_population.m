0024fc39662046b814b011a9f1d835412fe9903e
ayeletsa <ayeletsarel@gmail.com>
2020-07-27 09:33:17 +0300

    c

---
+++ Population_analysis/inter_field_zscore_population.m
@@ -0,0 +1,326 @@
+%% Population per field z score
+clear
+%general params:
+sort_by_max=1;
+
+%% data:
+dir_data='D:\Ayelet\2bat_proj\Analysis\new_code\analysis_structs\co_solo_initial_analysis\';
+dir_info=dir(dir_data);
+analysis_struct_folder='D:\Ayelet\2bat_proj\Analysis\new_code\analysis_structs\population\';
+
+
+co_shuffle_structs_folder = 'D:\Ayelet\2bat_proj\Analysis\new_code\analysis_structs\co_shuffling_struct\';
+figure_folder='D:\Ayelet\2bat_proj\Analysis\new_code\figures\population\';
+
+inclusion_dir='D:\Ayelet\2bat_proj\Analysis\new_code\analysis_structs\inclusion_cells_struct\';
+inclusion_dir_info=dir(inclusion_dir);
+inclusion_names={inclusion_dir_info.name};
+inclusion_names([inclusion_dir_info.isdir])=[];
+cell_ind=regexp(inclusion_names{2},'cell_');
+
+cell_nums_inclusion=(cellfun(@(c) c(cell_ind+5:cell_ind+7),inclusion_names,'UniformOutput',false));
+
+%% params for valid cells during CO
+param_folder='D:\Ayelet\2bat_proj\Analysis\new_code\params\';
+pop_param_file_name=fullfile(param_folder,'co_population_params.mat');
+load(pop_param_file_name)
+per_field_param_file_name=fullfile(param_folder,'per_field_params.mat');
+load(per_field_param_file_name)
+per_field_param_file_name=fullfile(param_folder,'per_field_params.mat');
+load(per_field_param_file_name)
+
+%% initialize:
+per_field_cell_dir_count=0;
+fr_count_all=0;
+cell_dir_per_field_count=0;
+
+all_per_field_valid_tuning=[];
+all_per_field_valid_tuning_zscore=[];
+all_pos_width=[];
+all_neg_width=[];
+all_compound_width=[];
+all_compound_pos_width=[];
+all_compound_neg_width=[];
+all_pos_rise_time=[];
+all_neg_rise_time=[];
+%% TO DO:
+%check why there are large fields
+%hist of per field sizes
+% set minimum spikes for per field analysis
+%%
+file_names={dir_info.name};
+file_names=file_names(find([dir_info.isdir]==0));
+
+%% 1. run over cells:
+for cell_i=1:length(file_names)
+    % load data:
+    load(fullfile(dir_data,file_names{cell_i}))
+    cell_num=cell_co_solo_initial_analysis.exp_data.cell_num;
+    %load cell's inclusion:
+    Match=cellfun(@(a) find(contains(a,num2str(cell_num))) , cell_nums_inclusion, 'UniformOutput', 0);
+    r=find(cellfun(@(c) ~isempty(c),Match));
+    load(fullfile(inclusion_dir,inclusion_names{r}))
+    
+    
+    for dir_i=1:2
+        
+       % basic cell data:
+        bat=cell_co_solo_initial_analysis.exp_data.bat;
+        day=cell_co_solo_initial_analysis.exp_data.day;
+        cell_num=cell_co_solo_initial_analysis.exp_data.cell_num;
+        %ego tuning of the cell:
+        all_ego_tuning(cell_i,dir_i,:)=cell_co_solo_initial_analysis.co(dir_i).firing_rate.dis_m(1,:);
+       
+        %% ego signif cell
+ 
+%         shuffle_struct_name = ['co_shuffling_struct_b',num2str(bat),'_d',num2str( day),'_c',num2str(cell_num),'.mat'];
+%         shuffle_file_name = fullfile(co_shuffle_structs_folder,shuffle_struct_name);
+
+        ego_signif_cells(cell_i,dir_i)=inclusion(dir_i).ego_cell;
+        
+        %% per field data
+        %1. Test if run on per field:
+       
+%         a=sum(~isnan(cell_co_solo_initial_analysis.solo(dir_i).spikes.ts_usec(:)))+sum(~isnan(cell_co_solo_initial_analysis.co(dir_i).spikes.ts_usec(:)))>=min_n_spike;
+%         b=cell_co_solo_initial_analysis.solo(dir_i).SI>SI_threshold;
+%         c=~isempty(cell_co_solo_initial_analysis.solo(dir_i).fields);
+%         d=cell_co_solo_initial_analysis.exp_data.mean_fr<max_for_pyramidal;
+%         per_field_cell_cond=a & b & c & d;
+        if inclusion(dir_i).place_cell 
+          cell_dir_per_field_count=cell_dir_per_field_count+1;
+
+            inter_field_data=cell_co_solo_initial_analysis.co(dir_i).inter_field;
+           % solo_field_data=cell_co_solo_initial_analysis.solo(dir_i).fields;
+            
+          
+
+          for field_i=1:length(inter_field_data)
+              % run only on per fields that obey conditions:
+              r=inter_field_data(field_i).tuning_dis_x_fr_per_field;
+              [ind_length,~,~]=find_length_of_consecutive_ind(find(~isnan(r)),length(r));
+              a=max(ind_length)>=min_r_length_per_field*length(r); %enough data that is not nan in tuning curve
+              b=inter_field_data(field_i).number_of_spikes_per_field>=min_n_spike_per_field;% enough spikes
+              per_field_cond=a & b;
+               if per_field_cond
+                fr_count_all=fr_count_all+1;
+                %1. per field tuning:
+                all_per_field_valid_tuning(fr_count_all,:)=nan*zeros(size(r));
+                all_per_field_valid_tuning(fr_count_all,:)=r';
+                non_nan_ind=~isnan(r);
+                all_per_field_valid_tuning_zscore(fr_count_all,:)=nan*zeros(size(r));
+                all_per_field_valid_tuning_zscore(fr_count_all,non_nan_ind)=zscore(r(non_nan_ind))';
+                
+                % solo_fe
+%                 % iso dis:
+%                 Isolation_dis(cell_dir_per_field_count)=cell_co_solo_initial_analysis.exp_data.Isolation_dis;
+% 
+%                 %2. get solo prop per field:
+                % per_field_solo_height(fr_count_all)=solo_field_data(field_i).peak;
+%                 per_field_solo_nrm_height(fr_count_all)=solo_field_data(field_i).peak./nanmean(cell_co_solo_initial_analysis.solo(dir_i).PSTH_for_field_detection);
+%                 per_field_solo_size(fr_count_all)=solo_field_data(field_i).width_href;
+%                 per_field_solo_SI(fr_count_all)=solo_field_data(field_i).field_SI;
+%                  
+%                 %3. get co prop per field:
+%                 per_field_co_SI(fr_count_all)=per_field_data(field_i).SI;
+%                 per_field_co_CV(fr_count_all)=per_field_data(field_i).cv;
+%                 per_field_co_modulation_depth(fr_count_all)=per_field_data(field_i).modulation_depth;
+%                 per_field_co_sparsity(fr_count_all)=per_field_data(field_i).sparsity;
+%                 
+                %4. get tuning width and rise time:
+                if inter_field_data(field_i).time_signif_field.signif_based_on_extreme_bins==1 
+                    rise_and_width_data=get_per_field_tuning_width_and_rise_time(inter_field_data(field_i).time_signif_field,min_dis_pos_neg);
+                    id_per_field_non_signif(fr_count_all)=0;
+                    id_per_field_pos_tuning(fr_count_all)=rise_and_width_data.pos_per_field_tuning;
+                    id_per_field_neg_tuning(fr_count_all)=rise_and_width_data.neg_per_field_tuning;
+                    
+% 
+%                     all_pos_width=[all_pos_width,rise_and_width_data.pos_width];
+%                     all_neg_width=[all_neg_width,rise_and_width_data.neg_width];
+%                     all_compound_width=[all_compound_width,rise_and_width_data.compound_width];
+%                     all_compound_pos_width=[all_compound_pos_width,rise_and_width_data.pos_compound_width];
+%                     all_compound_neg_width=[all_compound_neg_width,rise_and_width_data.neg_compound_width];
+%                     all_pos_rise_time=[all_pos_rise_time,rise_and_width_data.pos_rise_time];
+%                     all_neg_rise_time=[all_neg_rise_time,rise_and_width_data.neg_rise_time];
+                    
+              
+                else
+                    id_per_field_non_signif(fr_count_all)=1;
+                    id_per_field_pos_tuning(fr_count_all)=0;
+                    id_per_field_neg_tuning(fr_count_all)=0;
+                  
+                end
+               end
+          end
+%           % compute cell per field corr:
+%           if size(all_per_field_valid_tuning,1)>1
+%               per_field_cell_dir_count=per_field_cell_dir_count+1;
+%               corrs=tril(corr(all_per_field_valid_tuning',all_per_field_valid_tuning','rows','pairwise'),-1);
+%               corr_per_field_per_cell(per_field_cell_dir_count)=nanmean(corrs(corrs~=0));
+%           
+%           end
+        end
+    end
+%     %% compute corr between dirs:
+%     corrs=tril(corr(squeeze(all_ego_tuning(cell_i,:,:))','rows','pairwise'),-1);
+%     corr_ego_tuning_between_dirs(cell_i)=corrs(2,1);
+end
+%    %% divde solo fr to 4 subdevisions:
+%    marings=prctile(per_field_solo_height,[25,50,75]);
+%    field_group{1}=find(per_field_solo_height<marings(1));
+%    field_group{2}=find(per_field_solo_height>marings(1) & per_field_solo_height<marings(2));
+%    field_group{3}=find(per_field_solo_height>marings(2) & per_field_solo_height<marings(3));
+%    field_group{4}=find(per_field_solo_height>marings(3));
+id_per_field_non_signif=logical(id_per_field_non_signif);
+id_per_field_pos_tuning=logical(id_per_field_pos_tuning);
+id_per_field_neg_tuning=logical(id_per_field_neg_tuning);
+
+%% plot
+figure('units','normalized','outerposition',[0 0 1 1])
+mat_size=[0.08 0.08];
+tuning_size=[0.05 0.06];
+
+hor_dist=0.095;
+x_pos(1)=0.03;
+x_pos(2)=x_pos(1)+hor_dist;
+x_pos(3)=x_pos(2)+hor_dist;
+x_pos(4)=x_pos(3)+hor_dist;
+x_pos(5)=x_pos(4)+hor_dist;
+x_pos(6)=x_pos(5)+hor_dist;
+x_pos(7)=x_pos(6)+hor_dist;
+x_pos(8)=x_pos(7)+hor_dist;
+x_pos(9)=x_pos(8)+hor_dist;
+x_pos(10)=x_pos(9)+hor_dist;
+
+ver_dist=0.2;
+y_pos_mat(1)=0.83;
+y_pos_mat(2)=y_pos_mat(1)-ver_dist;
+y_pos_mat(3)=y_pos_mat(2)-ver_dist;
+y_pos_mat(4)=y_pos_mat(3)-ver_dist;
+y_pos_mat(5)=y_pos_mat(4)-ver_dist;
+%%
+ylim_tuning=[0 15];
+y_lim_zscore=[-1 1.5];
+% tuning:
+ylimits=ylim_tuning;
+ix=1;
+iy=1;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning;
+title_str='tuning of all valid inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+ix=1;
+iy=2;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning(~id_per_field_non_signif,:);
+title_str='tuning of signif inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+
+ix=1;
+iy=3;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning(id_per_field_pos_tuning,:);
+title_str='tuning of pos signif inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+
+ix=1;
+iy=4;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning(id_per_field_neg_tuning,:);
+title_str='tuning of neg signif inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+
+id_compound=intersect(find(id_per_field_neg_tuning),find(id_per_field_pos_tuning));
+ix=1;
+iy=5;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning(id_compound,:);
+title_str='tuning of compound inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+
+%zcore
+ylimits=y_lim_zscore;
+ix=2;
+iy=1;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning_zscore;
+title_str='zscore tuning of all valid inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+ix=2;
+iy=2;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning_zscore(~id_per_field_non_signif,:);
+title_str='zscore tuning of signif inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+ix=2;
+iy=3;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning_zscore(id_per_field_pos_tuning,:);
+title_str='zscore tuning of pos signif inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+
+ix=2;
+iy=4;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning_zscore(id_per_field_neg_tuning,:);
+title_str='zscore tuning of neg signif inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+
+ix=2;
+iy=5;
+pos_ax_mat=[x_pos(ix),y_pos_mat(iy),mat_size];
+pos_ax_tuning=[x_pos(ix),y_pos_mat(iy)+mat_size(2),tuning_size];
+data=all_per_field_valid_tuning_zscore(id_compound,:);
+title_str='zscore tuning of compound inter fields';
+plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+
+
+%% save
+fig_file_name=['D:\Ayelet\2bat_proj\Analysis\new_code\figures\population\inter_field_zscore_color_bar_sort_by_',num2str(sort_by_max),'.jpg'];
+saveas(gcf,fig_file_name)
+
+
+%%
+function plot2dmats(pos_ax_mat,pos_ax_tuning, data, dis_per_field_bin_vec_of_center,title_str,sort_by_max,ylimits)
+normalize_to_other_max=[];
+%[xlimits, ylimits] = fn_plot_2D_field (data, dis_per_field_X_bins_vector,dis_per_field_bin_vec_of_center, cells_bin,cells_bin_vector_of_centers,normalize_to_other_max);
+axes('position',pos_ax_mat)
+if sort_by_max==1
+[~,id]=max(data,[],2);
+else
+  [~,id]=min(data,[],2);
+end
+  
+[~,idd]=sort(id);
+data=data(idd,:);
+imagesc(data)
+xlabel('Inter-bat distance (m)')
+set(gca,'xtick',0:10:length(dis_per_field_bin_vec_of_center), 'xticklabel',[-40 -20 0 20 40])
+%ylabel('Fields')
+colormap(jet)
+colorbar('location','eastoutside')
+
+axes('position',pos_ax_tuning)
+plot(dis_per_field_bin_vec_of_center,nanmean(data))
+title(title_str)
+ylim(ylimits)
+xlim([-40 40])
+set(gca,'xtick',[])
+end
---
+++ poster_figures/inter_field_cells_examples_more_details.m
@@ -0,0 +1,297 @@
+
+% plot imporatnat statistics for cross-overs analysis
+close all
+clear
+clc
+load('D:\Ayelet\2bat_proj\Analysis\new_code\params\co_params.mat')
+load('D:\Ayelet\2bat_proj\Analysis\new_code\params\per_field_params.mat')
+
+
+% cells_num_dir=[267 2;257 2;261 2; 277 2;238 1; 296 2];
+% relevant_field{1}=[1,2,3];
+% relevant_field{2}=[1,2];
+% relevant_field{3}=[1,2,3];
+% relevant_field{4}=[1,2];
+% relevant_field{5}=[1,2];
+% relevant_field{6}=[1,2,3];
+cells_num_dir=[298,2;296,2;274,2;251,2;238,1;235,2;226,1;211,1;531,1;525,1;516,1;494,1;474,1;456,1;808,2;777,1;764,1;725,1;719,1;...
+    713,2;695,2;648,2;639,1;782,1;724,1];
+relevant_field{1}=[3];
+relevant_field{2}=[2,3];
+relevant_field{3}=[2];
+relevant_field{4}=[1];
+relevant_field{5}=[1];
+relevant_field{6}=[2,3];
+relevant_field{7}=[1,2];
+relevant_field{8}=[2];
+relevant_field{9}=[1];
+relevant_field{10}=[1];
+relevant_field{11}=[2,3];
+relevant_field{12}=[2];
+relevant_field{13}=[2];
+relevant_field{14}=[1];
+relevant_field{15}=[3];
+relevant_field{16}=[2];
+relevant_field{17}=[2];
+relevant_field{18}=[3];
+relevant_field{19}=[3];
+relevant_field{20}=[1];
+relevant_field{21}=[1];
+relevant_field{22}=[4];
+relevant_field{23}=[2];
+relevant_field{24}=[2];
+relevant_field{25}=[3];
+
+%% structs' parameters
+
+behavior_structs_folder = 'D:\Ayelet\2bat_proj\Analysis\new_code\analysis_structs\co_solo_initial_analysis\';
+files = dir(behavior_structs_folder);
+behavior_struct_names = {files.name};
+co_shuffle_structs_folder = 'D:\Ayelet\2bat_proj\Analysis\new_code\analysis_structs\co_shuffling_struct\';
+files = dir(co_shuffle_structs_folder);
+co_shuffle_struct_names = {files.name};
+
+
+%% figure parameters
+
+spike_colors = {[1  0 1],[1  0 1],[.95 .95 .95]};
+solo_colors = {[0 0 0],[0 0 0]};
+FR_colors = {[1 1 1],[1 1 1]};%
+coherence_colors = {[1 .5 0],[.8 0 .6]};
+tunnel_limits = [0 130];
+sig_bkg_color = [.8 .95 1];
+lwidth = 3;
+alpha = .05;
+
+
+%% open figure
+
+figure('units','normalized','outerposition',[0 0 1 1])
+paper_size = [24 21]; % ~A4 size
+set(gcf,'DefaultAxesFontSize',12);
+set(gcf,'DefaultAxesFontName','helvetica');
+set(gcf,'PaperUnits','centimeters','PaperPosition',[0 0 paper_size]);
+set(gcf,'PaperOrientation','landscape');
+set(gcf,'Units','centimeters','Position',get(gcf,'paperPosition')+[1 1 0 0]); % position on screen...
+set(gcf, 'Renderer', 'opengl');
+
+fsize = 14;
+y_pos=0.4;
+x_solo_psth=0.6;
+x_solo_raster=0.545;
+x_co_raster=0.35;
+x_co_2d=0.15;
+x_per_field=0.08;
+
+
+%% plot for each cell
+
+for ii_cell = 1:length(cells_num_dir)
+    fields_to_plot=relevant_field{ii_cell};
+    num_fileds=length(fields_to_plot);
+    
+    
+    co_tun_ax = axes('units','normalized','Position',[x_co_raster y_pos+0.175 0.18 0.06]);
+    co_raste_ax = axes('units','normalized','Position',[x_co_raster y_pos 0.18 0.17]); %
+    co_2d_ax = axes('units','normalized','Position',[x_co_raster y_pos-0.175 0.18 0.17]); %
+    solo_raster_ax = axes('units','normalized','Position',[x_solo_raster y_pos 0.05 0.17]); % allo fr comparison
+    solo_psth_ax = axes('units','normalized','Position',[x_solo_psth y_pos 0.05 0.17]); % allo fr comparison
+    
+    %% load data
+    cell_file_ind=find(~cellfun(@isempty,regexp(behavior_struct_names,sprintf('cell_%d.mat',cells_num_dir(ii_cell,1)))));
+    ii_dir=cells_num_dir(ii_cell,2);
+    
+    struct_name =behavior_struct_names{cell_file_ind};
+    file_name = fullfile(behavior_structs_folder,struct_name);
+    load(file_name);
+    behavior_struct=cell_co_solo_initial_analysis;
+    bat=behavior_struct.exp_data.bat;
+    day=behavior_struct.exp_data.day;
+    cell_num=behavior_struct.exp_data.cell_num;
+    
+    shuffle_struct_name = ['co_shuffling_struct_b',num2str(bat),'_d',num2str( day),'_c',num2str(cell_num),'.mat'];
+    file_name = fullfile(co_shuffle_structs_folder,shuffle_struct_name);
+    if exist(file_name)
+        co_shuffle_struct=load(file_name);
+        co_shuffle_struct=co_shuffle_struct.shuffling_struct;
+        
+        dis_before_after_co = 40;
+        %% title
+        axes('units','normalized','Position',[0.1 0.1 0.7 0.7]);
+        text(0.23,0.95,sprintf('Cell %d',cell_num),'fontsize',fsize*1.3,'fontweight','bold')
+        text(0.63,0.8, 'Solo data','fontsize',fsize,'fontweight','bold')
+        text(0.37,0.8,'Cross-over data','fontsize',fsize,'fontweight','bold')
+        text(-0.05,0.8,sprintf('Cross-over data\nwithin the "inter-fields"'),'fontsize',fsize,'fontweight','bold')
+        box off
+        axis off
+         %% 2D raster CO
+        axes(co_raste_ax)
+        hold on
+        line([0 0],tunnel_limits,'Color',[.7 .7 .7],'LineStyle','--')
+        plot(behavior_struct.co(ii_dir).bsp.dis_m(:),behavior_struct.co(ii_dir).bsp.x_pos(:),'.','color',spike_colors{3})
+        plot(behavior_struct.co(ii_dir).spikes.dis_m(:),behavior_struct.co(ii_dir).spikes.x_pos(:),'.','color',spike_colors{ii_dir},'markersize',8)
+        set(gca,'xlim',[-dis_before_after_co dis_before_after_co],...
+            'ylim',tunnel_limits,...
+            'ytick',tunnel_limits(2))
+        text(-dis_before_after_co-20,tunnel_limits(2)/2, sprintf('Position\nin tunnel (m)'),'fontsize',fsize,'Rotation',90,'HorizontalAlignment','center')
+        set(gca,'xtick',[])
+        box off
+        %% CO firing rate
+        axes(co_tun_ax)
+        hold on
+        plot(co_shuffle_struct(ii_dir).shuffled_data.ego_bin_centers,co_shuffle_struct(ii_dir).shuffled_data.ego_firing_rate(2:end,:),'color',spike_colors{3},'LineWidth',lwidth);
+        plot(co_shuffle_struct(ii_dir).shuffled_data.ego_bin_centers,co_shuffle_struct(ii_dir).shuffled_data.ego_firing_rate(1,:),'color',spike_colors{ii_dir},'LineWidth',lwidth);
+        max_y = round(max(max(co_shuffle_struct(ii_dir).shuffled_data.ego_firing_rate))*1.1*10) / 10;
+        if max_y < 1
+            max_y = 1;
+        end
+        bins_diff = mean(diff(co_shuffle_struct(ii_dir).shuffled_data.ego_bin_centers));
+        x_limits = [co_shuffle_struct(ii_dir).shuffled_data.ego_bin_centers(1)-bins_diff/2 co_shuffle_struct(ii_dir).shuffled_data.ego_bin_centers(end)+bins_diff/2];
+        set(gca,'xlim',x_limits,'XTick',[],'ylim',[0 max_y],'Ytick',max_y,'color',FR_colors{1});
+        str = sprintf('Hz');
+        text(-dis_before_after_co-10,max_y/2, str,'fontsize',fsize,'Rotation',90,'HorizontalAlignment','center')
+
+        box off
+        %% co 2d
+        axes(co_2d_ax)
+        hold on
+        bin_dis_i=1;
+        bin_allo_i=1;
+        if cells_num_dir(ii_cell,1)==235
+                  normalize_to_other_max=4;
+
+        else
+      normalize_to_other_max=[];
+        end
+         normalize_to_other_max=[];
+        field_density_mat_X_Y=behavior_struct.co(ii_dir).firing_rate.field_density_smoothed_XY_with_NaN;
+        X_bins_vector=dis_X_bins_vector_2D;
+        X_bins_vector_of_centers=dis_X_bins_vector_of_centers_2D;
+        Y_bins_vector=allo_X_bins_vector_2D;
+        Y_bins_vector_of_centers=allo_X_bins_vector_of_centers_2D;
+        allo_bin_size=allo_X_bin_size_2D;
+        dis_bin_size=dis_X_bin_size_2D;
+        
+        [xlimits, ylimits] = fn_plot_2D_field (field_density_mat_X_Y, X_bins_vector, X_bins_vector_of_centers,Y_bins_vector, Y_bins_vector_of_centers,normalize_to_other_max);
+        set(gca,'xlim',xlimits,...
+            'ylim',tunnel_limits,...
+            'ytick',tunnel_limits)
+        
+        text(-dis_before_after_co-20,tunnel_limits(2)/2, sprintf('Position\nin tunnel (m)'),'fontsize',fsize,'Rotation',90,'HorizontalAlignment','center')
+        xlabel('Inter-bat distance (m)','fontsize',fsize)
+        max_fr=max(field_density_mat_X_Y(:));
+        if cells_num_dir(ii_cell,2)==1
+            text(xlimits(1),0.95*ylimits(2),sprintf('%.1f Hz',max_fr))       
+        else
+            text(xlimits(2)*0.4,0.95*ylimits(2),sprintf('%.1f Hz',max_fr))
+        end
+        box off
+        
+        
+        
+        %% raster of solo spikes
+        inter_field=behavior_struct.co(ii_dir).inter_field;
+        per_field=behavior_struct.co(ii_dir).per_field_href;
+        x=[behavior_struct.solo(ii_dir).fields.edges_href];
+        field_edges=reshape(x,2,length(x)/2);
+        inter_field_edges=inter_field.inter_field_edge;
+        
+        axes(solo_raster_ax)
+        hold on
+        plot(behavior_struct.solo(ii_dir).spikes.ts_usec(:),behavior_struct.solo(ii_dir).spikes.x_pos(:),'.','color','k','markersize',8)
+        time_limits = [min(behavior_struct.solo(ii_dir).bsp.ts_usec(:)) max(behavior_struct.solo(ii_dir).bsp.ts_usec(:))];
+        time_limits_str=round([time_limits-time_limits(1)]*1e-6*(1/60));
+        plot(repmat([time_limits(1);time_limits(1)],1,size(field_edges,2)),field_edges,'color',[.5 .5 .5],'linewidth',2)
+        plot(repmat([time_limits(1);time_limits(1)],1,size(inter_field_edges,2)),inter_field_edges,'color',[0 0 1],'linewidth',2) 
+        plot(time_limits(1)*ones(1,length(fields_to_plot)),mean(inter_field_edges(:,fields_to_plot)),'r.')
+        set(gca,'xlim',time_limits,'xtick',time_limits,'xticklabel',time_limits_str,'ylim',tunnel_limits,'ytick',[])
+        xlabel ({'Time';'(min)'},'fontsize',fsize)
+        
+        %% solo firing rate - regular bins
+        axes(solo_psth_ax)
+        hold on
+        %plot(co_shuffle_struct(ii_dir).shuffled_data.allo_firing_rate(2:end,:),co_shuffle_struct(ii_dir).shuffled_data.allo_bin_centers,'color',spike_colors{3},'LineWidth',lwidth);
+        plot(behavior_struct.solo(ii_dir).PSTH_for_field_detection,behavior_struct.solo(1).x_pos_firing_rate{1, 2},'color',solo_colors{ii_dir},'LineWidth',2);
+        max_x = 1.2*ceil(max(behavior_struct.solo(ii_dir).PSTH_for_field_detection));
+        plot(behavior_struct.solo(ii_dir).field_height+0.1*max_x,behavior_struct.solo(ii_dir).field_center,'*r')
+        if max_x~=0 & ~isnan(max_x)
+            set(gca,'ylim',tunnel_limits,'YTick',[],'xlim',[0 max_x],'Xtick',max_x,'color',FR_colors{2});
+        end
+        str = sprintf('Hz');
+        xlabel(str,'fontsize',fsize)
+        box off
+        
+        
+        
+        %% per field firing rate:
+        axes('units','normalized','Position',[x_per_field y_pos 0.18 0.17]);
+        text(0.5,-0.3,'Inter-bat distnace (m)','fontsize',fsize,'horizontalalignment','center')
+        text(-0.3,0.5,'Hz','fontsize',fsize,'rotation',90,'horizontalalignment','center')
+        box off
+        axis off
+        if num_fileds>0
+            % sorted_hights_ind_sorted=sort(sorted_hights_ind);
+            dis_y_pos=0.06;
+            y_pos_vec(1)=y_pos;
+            y_pos_vec(2)=y_pos_vec(1)+dis_y_pos;
+            y_pos_vec(3)=y_pos_vec(2)+dis_y_pos;
+            y_pos_vec(4)=y_pos_vec(3)+dis_y_pos;
+            count=0;
+            for fr_i=sort(fields_to_plot)
+                count=count+1;
+                
+                axes('units','normalized','Position',[x_per_field y_pos_vec(count) 0.18 0.05]);
+                if sum(~isnan(behavior_struct.co(ii_dir).inter_field(fr_i).tuning_dis_x_fr_per_field))~=0
+                    bins_center=dis_per_field_bin_vec_of_center;
+                    shuf_data=behavior_struct.co(ii_dir).inter_field(fr_i).dis_signif_field.shuffled_data;
+                    r=behavior_struct.co(ii_dir).inter_field(fr_i).tuning_dis_x_fr_per_field ;
+                    plot(bins_center,shuf_data,'color',spike_colors{3},'LineWidth',lwidth); hold on;
+                    plot(bins_center ,r,'color',spike_colors{ii_dir},'LineWidth',lwidth);
+                    if behavior_struct.co(ii_dir).inter_field(fr_i).dis_signif_field.signif_based_on_extreme_bins==1
+                        neg_signif=behavior_struct.co(ii_dir).inter_field(fr_i).dis_signif_field.neg_signif;
+                        plot(bins_center(neg_signif),r(neg_signif),'ro')
+                        pos_signif=behavior_struct.co(ii_dir).inter_field(fr_i).dis_signif_field.pos_signif;
+                        plot(bins_center(pos_signif),r(pos_signif),'go')
+                    end
+                   
+                    max_y = round(max(max([r;shuf_data]))*1.1*10) / 10 +1;
+                    if max_y < 1
+                        max_y = 1;
+                    end
+                    bins_diff = mean(diff(co_shuffle_struct(ii_dir).shuffled_data.ego_bin_centers));
+                    x_limits = [co_shuffle_struct(ii_dir).shuffled_data.ego_bin_centers(1)-bins_diff/2 co_shuffle_struct(ii_dir).shuffled_data.ego_bin_centers(end)+bins_diff/2];
+                    if count==1
+                        set(gca,'xlim',x_limits,'ylim',[0 max_y],'Ytick',max_y,'color',FR_colors{1});
+                        
+                    else
+                        set(gca,'xlim',x_limits,'XTick',[],'ylim',[0 max_y],'Ytick',max_y,'color',FR_colors{1});
+                        
+                    end
+                   % r=behavior_struct.co(ii_dir).firing_rate.dis_x_fr_per_field{1, fr_i};
+                    
+                    box off
+                end
+            end
+        end
+        
+        
+        
+        
+        
+    end
+    
+    %% save figure
+    
+    fig = gcf;
+    %fig.InvertHardcopy = 'off';
+    dir_name=['D:\Ayelet\2bat_proj\Analysis\new_code\figures\poster_figures\'];
+    if ~exist(dir_name)
+        mkdir(dir_name)
+    end
+    fig_name=fullfile(dir_name,['inter_field_egocentric_cell_',num2str(cell_num),'_dir_',num2str(ii_dir)]);
+    print(gcf, fig_name, '-dpdf',  '-painters');
+    fig_name_fig=[fig_name,'.fig'];
+    saveas(gcf,fig_name_fig)
+    clf
+    
+end
